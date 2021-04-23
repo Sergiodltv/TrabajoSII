c9243bc12cf577eef84aa5bb3afb502b2c7fbe18
PGarciaLopez <0619883086@uma.es>
2021-04-23 11:05:47 -0700

    Version 2.10

--- src/main/java/practicaSII/ejb/clasesejb/ImportarAlumnosEJB.java
+++ src/main/java/practicaSII/ejb/clasesejb/ImportarAlumnosEJB.java
@@ -11,6 +11,9 @@ import java.util.Scanner;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
 public class ImportarAlumnosEJB {
 	private String fichero;
 	AlumnosEJB ejb;
@@ -20,15 +23,32 @@ public class ImportarAlumnosEJB {
 		ejb = new AlumnosEJB();
 	}
 	
-	public void insertarDatos() {
+	public void insertarDatos() throws CsvValidationException {
 		try {
 			Reader r = new FileReader(fichero);
-			Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(r);
+			/*Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(r);
 			for(CSVRecord record:records) {
 				String dni = record.get(0);
 				String nombre = record.get(1);
 				String ap1 = record.get(2);
 				String ap2 = record.get(3);
+			}*/
+			
+			
+			CSVReader csvReader = new CSVReader (r);
+			String[] fila = null;
+			while((fila = csvReader.readNext()) != null) {
+				String dni = fila[0];
+				String nombre = fila[1];
+				String ap1 = fila[2];
+				String ap2 = fila[3];
+				String eminst = fila[7];
+				String empers = fila[8];
+				String telefono = fila[9];
+				String movil = fila[10];
+				String direccion = fila[11];
+				
+				System.out.println(dni+" | "+nombre+" | "+ap1+" | "+ap2+" | "+eminst+" | "+empers+" | "+telefono+" | "+movil+" | "+direccion);
 			}
 		}catch(FileNotFoundException e) {
 			
